4f5b368aba46b2728e44c6622e662ed7ef21a5c3
Violetta-Odagiu <violettaodagiu@gmail.com>
2021-03-15 20:36:07 +0200

    initial commit

---
+++ SecondProgram.java
@@ -0,0 +1,5 @@
+public class SecondProgram {
+    public static void main(String[] argumente){
+        System.out.println("Exercitii" +argumente[0]+" "+argumente[1]+" "+argumente[2]+" "+argumente[3]+
+
+    }
